ab31cc90462fb9c554f4cc4294f9df07dcdd839e
João Gabriel <joaogabriel-12@hotmail.com>
2023-12-22 00:16:16 -0300

    chore: add cors config

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ const path = require('path')
 
 const app = express()
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}
+app.use(cors())
 
-app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -20,7 +15,9 @@ app.use('/app', express.static(path.join(__dirname, 'public')))
 
 const apiRouter = require('./api/routes/apiRouter')
 
-app.use('/api', cors(corsOptions), apiRouter)
+app.use('/api', apiRouter)
 
 const port = process.env.PORT || 5432
-app.listen(port)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
